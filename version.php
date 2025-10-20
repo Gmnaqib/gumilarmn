828b8cfffd748db2bdce87daa311276e9b6c2831
Gmnaqib <gumilarmn16@gmail.com>
2025-10-21 02:15:20 +0700

    Fix missing closing brace in mobile class view_gumilar method

--- version.php
+++ version.php
@@ -3,8 +3,8 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_gumilar';
-$plugin->version = 2025102103; // YYYYMMDDHH - Fixed button functionality
+$plugin->version = 2025102104; // YYYYMMDDHH - Fixed syntax errors
 $plugin->requires = 2022041900; // Moodle 4.0
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '1.3';
+$plugin->release = '1.4';
 $plugin->dependencies = array();
